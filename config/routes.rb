5f86515713710c2ea01c4e8b659564d4bffeae98
shota-nagato <shota.nagato@gmail.com>
2024-07-23 23:42:27 +0900

    dashboardページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", :as => :rails_health_check
+  authenticate :user do
+    root to: "dashboard#show", as: :user_root
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
