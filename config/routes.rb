3d23b676cbfce9663273304b8ceab2fd88773692
shota-nagato <shota.nagato@gmail.com>
2024-07-24 23:14:50 +0900

    registrationsをカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      registrations: "users/registrations"
+    }
 
   authenticate :user do
     root to: "dashboard#show", as: :user_root
