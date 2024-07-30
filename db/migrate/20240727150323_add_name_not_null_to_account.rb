ae8abf96ac568f5a9657d745f74b233808443786
shota-nagato <shota.nagato@gmail.com>
2024-07-31 00:04:18 +0900

    not null制約追加

---
+++ db/migrate/20240727150323_add_name_not_null_to_account.rb
@@ -0,0 +1,5 @@
+class AddNameNotNullToAccount < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :accounts, :name, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_22_145052) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_28_005028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_22_145052) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.bigint "account_id"
+    t.bigint "account_id", null: false
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin", default: false, null: false
